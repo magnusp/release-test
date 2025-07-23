97d4f0e28fedd1f290e7d9b0c0c4f738cc33c94d
Magnus Persson <magnus.persson@fortnox.se>
2025-07-23 09:16:06 +0200

    Patch bump

--- src/main/java/io/github/magnusp/Main.java
+++ src/main/java/io/github/magnusp/Main.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		List.of(1, 2, 3, 4).forEach(System.out::println);
+		List.of(1, 2, 3, 4, 5).forEach(System.out::println);
 	}
 }
